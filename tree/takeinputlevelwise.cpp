fff411f85402ad1d75a43d648b8d77662654eab1
aadi2525 <aadirajhodlurkar@gmail.com>
2024-02-17 14:55:39 +0530

    17-feb

---
+++ tree/takeinputlevelwise.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class Trees{
+    public:
+    T data;
+    vector<Trees<T>*> children;
+
+    Trees(T data){
+        this->data = data;
+    }
+
+
+};
+
+void pprint(Trees<int>* root){
+    cout<<root->data<<" :";
+    for(int i=0;i<root->children.size();i++){
+        cout<<root->children[i]->data<< " ," ;
+    }
+    cout<<endl;
+
+    for(int i =0;i<root->children.size();i++){
+       pprint(root->children[i]);
+    }
+
+}
+
+Trees<int>* levelInpput(){
+    int rootdata;
+    cout<<"enter a rootdaata"<<endl;
+    cin>>rootdata;
+    Trees<int>* root = new Trees<int> (rootdata);
+
+    queue<Trees<int>*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Trees<int>* f = q.front();
+        q.pop();
+
+        cout<<"Enter number of  childrens of "<<f->data<<endl;
+        int n;
+        cin>> n;
+
+        for(int i=1;i<=n;i++){
+            int childdata;
+            cout<<"enter " <<i<< " th  child of "<<f->data << endl;
+            cin>>childdata;
+            Trees<int>* child = new Trees<int>(childdata);
+
+            q.push(child);
+            f->children.push_back(child);
+        }
+    }
+    
+return root;
+
+}
+
+
+int main(){
+
+    Trees<int>* root = levelInpput();
+    pprint(root);
+
+    return 0;
+}
