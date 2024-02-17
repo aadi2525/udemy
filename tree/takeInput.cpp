6e41919a639fac922b5c9d0ab7344588c73eee14
aadi2525 <aadirajhodlurkar@gmail.com>
2024-02-17 12:23:44 +0530

    17-feb

--- tree/takeInput.cpp
+++ tree/takeInput.cpp
@@ -16,7 +16,7 @@ void pprint(Trees<int>* root){
     cout<<root->data<<": ";
 
     for(int i =0 ;i<root->children.size();i++){
-        cout<<root->children[i]->data;
+        cout<<root->children[i]->data<<" , ";
     }
     cout<<endl;
     for(int i =0;i<root->children.size();i++){
